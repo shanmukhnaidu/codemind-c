032800815996aeaa9a369feb48f516db8cab27a9
shanmukhnaidu <98636191+shanmukhnaidu@users.noreply.github.com>
2022-02-17 18:02:18 +0530

    #include<stdio.h> int main() {     float n,m;     float avg,sum;     1<=n,m<=1000;     scanf("%f%f",&n,&m);     sum=n+m;     avg=(sum)/2;     printf("%.4f",avg);     return 0; }

---
+++ Find_the_average_of_two_numbers.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    float n,m;
+    float avg,sum;
+    1<=n,m<=1000;
+    scanf("%f%f",&n,&m);
+    sum=n+m;
+    avg=(sum)/2;
+    printf("%.4f",avg);
+    return 0;
+}
